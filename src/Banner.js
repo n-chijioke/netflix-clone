63d6a07fdbab7616aefaf0f3222037fb5b9e5200
Christopher <[nwachukwuchristopher92@gmail.com]>
2021-06-29 08:26:30 +0100

    origin

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
 import requests from './requests';
+import "./Banner.css";
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -19,15 +20,35 @@ function Banner() {
         fetchData();
     }, []);
 
-    console.log(movie)
+    console.log(movie);
+
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
 
     return (
-        <header className="banner">
-            <div className="banner_content"></div>
-            {" "}
-            {/* title */}
-            {/* div > 2 buttons }
-            {/* description */}
+        <header className="banner"
+        style={{
+            backgroundSize: "cover",
+            backgroundImage: `url(
+                "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+            )`,
+            backgroundPosition: "center center",
+        }}
+    >
+            <div className="banner__contents"></div>
+            <h1 className="banner__title">
+                {movie?.title || movie?.name || movie?.original_name}
+            </h1>
+            
+            <div className="banner__buttons">
+                <button className="banner__button">play</button>
+                <button className="banner__button">My List</button>
+            </div>
+
+            <h1 className="banner__description">
+            {truncate(movie?.overview, 150)}
+            </h1>
         </header>
     )
 }
